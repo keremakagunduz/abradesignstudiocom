b259f3f28c5bdb400563950370fc8fb6f405a3b8
keremakagunduz <keremakagunduz@gmail.com>
2026-02-26 21:56:08 +0300

    email phone icons

--- abraapp/src/components/Footer.jsx
+++ abraapp/src/components/Footer.jsx
@@ -15,11 +15,25 @@ const Footer = () => (
     <div class="max-w-screen-xl px-4 mx-auto overflow-hidden sm:px-6 lg:px-8">
 
 
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+    <path d="M12 13.41l-8.59-8.59A2 2 0 0 0 1 6v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-1.41-.59L12 13.41z" fill="none" stroke="currentColor" stroke-width="2"/>
+    <polyline points="3 6 12 13 21 6" fill="none" stroke="currentColor" stroke-width="2"/>
+</svg>
 
         <p className="mt-4 text-base leading-6 text-center text-gray-400">info@abradesignstudio.com</p>
 
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+    <path d="M22 16.92c-1.44 0-2.83-.37-4.06-1.07a17.53 17.53 0 0 1-8.78-8.78C5.45 6.83 5.08 5.44 5.08 4c0-1.1.9-2 2-2h2c.56 0 1.06.23 1.41.59l2.83 2.83c.39.39.59.93.59 1.41v2c0 1.1-.9 2-2 2H8c-.74 0-1.43.36-1.83.96-1.79 2.97-.79 5.79 2.41 8.09 3.07 2.15 6.18 2.22 8.73.95C19.82 15.8 22 15.36 22 16.92z" fill="none" stroke="currentColor" stroke-width="2"/>
+</svg>
 
         <p className="mt-4 text-base leading-6 text-center text-gray-400">+90 505 251 8358</p>
+
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+    <path d="M20 2H4a2 2 0 0 0-2 2v18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2zm0 20H4V4h16v18zm-8-12l8 6h-16l8-6zm0-1.41l-8 6V6zm4.5 9.91a1.5 1.5 0 0 1-1.5 1.5H8a1.5 1.5 0 0 1-1.5-1.5V14h8v4.5z" fill="none" stroke="currentColor" stroke-width="2"/>
+    <path d="M15 12c-1 0-1 .71-1 1s0 1 .5 1c.5 0 1-.71 1-1s-.5-1-1-1z" fill="none" stroke="currentColor" stroke-width="2"/>
+</svg>
+
         <p className="mt-1 text-base leading-6 text-center text-gray-400">
           Copyrights of all pictures published on this website are reserved by
           Abra Design Studio. No material published here can be copied, reproduced,
