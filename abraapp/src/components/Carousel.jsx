48df46515a6d19d7fd0eef6d1b2b494b4d5168ca
keremakagunduz <keremakagunduz@gmail.com>
2026-02-28 00:29:17 +0300

    contact icon

--- abraapp/src/components/Carousel.jsx
+++ abraapp/src/components/Carousel.jsx
@@ -51,9 +51,9 @@ const Carousel = ({ projects }) => {
         </article>
 
           <div className="absolute inset-0 bg-white bg-opacity-50 flex w-full h-fit place-self-center pt-24 pb-24">
-      <div className="flex absolute self-center justify-between text-4xl w-full">
+      <div className="flex absolute self-center justify-between text-4xl w-full h-screen self-end">
         <button onClick={goToPrevious} className={styles.navButton}>&lt;</button>
-            <svg className="max-w-12" xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 95 118.75" x="0px" y="0px">
+            <svg className="max-w-8" xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 95 118.75" x="0px" y="0px">
             <path d="M82.664,81.539a2.745,2.745,0,0,1-1.45,2.12L72.024,89.2a44.218,44.218,0,0,1-7.933.707c-14.785,0-30.433-7.085-43.22-19.871C5.766,54.932-1.381,
             35.836,1.706,18.884l5.543-9.19a2.741,2.741,0,0,1,2.119-1.45,1.948,1.948,0,0,1,1.692.787L25.608,27.813c.3.39.72,1.112.132,1.574l-8.024,6.3a1.891,1.891,0,
             0,0-.724,1.518c.1,6.6,5.022,15.407,13.162,23.547S47.1,73.814,53.7,73.916a1.909,1.909,0,0,0,1.519-.724l6.3-8.024c.461-.588,1.184-.17,1.573.132L81.877,79.848A1.951,
