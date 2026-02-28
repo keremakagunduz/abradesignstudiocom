74af93bd3f1b8e461f1eabe1cd9cfe69fd2e8318
keremakagunduz <keremakagunduz@gmail.com>
2026-02-28 21:25:46 +0300

    linkedin icon

--- abraapp/src/pages/Home.jsx
+++ abraapp/src/pages/Home.jsx
@@ -7,6 +7,7 @@ import { Link, useLocation } from "react-router-dom";
 import styles from '../home.css';
 import ScrollToTop from '../components/ScrollToTop';
 import { FaPhoneAlt } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
 const imageLoader = (src, width, quality) => {
   return `${src}?format=auto${quality ? `&quality=${quality}` : ''}&width=${width}`;
@@ -100,7 +101,12 @@ export default function Home() {
             />
             <h3 className="text-xl font-semibold text-gray-800">Başak Bakkaloğlu</h3>
             <p className="mt-2 text-base leading-6 text-gray-600 p-12">
-                Lisans eğitimini İ.T.Ü. Mimarlık Fakültesi, Mimarlık Bölümü’nde tamamladı. Mezuniyeti sonrasında, çeşitli mimarlık ofislerinde, kentsel tasarım, konut, ofis ve mağaza tasarımı gibi farklı fonksiyon ve ölçekler üzerine çalışma imkanı buldu, deneyim kazandı. Eş zamanlı olarak mobilya ve ürün tasarımına ağırlık verdi. Mimarlık, iç mimarlık ve ürün tasarımı çalışmalarını tek bir isim altında sürdürme isteğiyle, 2014 yılında Cemal Çobanoğlu ile “ABRA Design Studio”yu kurdu. Kadir Has Üniversitesi, MEF Üniversitesi ve Beykent Üniversitesi’nde temel tasarım, mobilya tasarımı, mobilya tarihi ve ileri sunum teknikleri dersleri verdi. Tasarımları yurt içinde ve yurt dışında pek çok farklı sergi ve yayında yer aldı. Mobilya tasarımı yarışmalarında çeşitli ödüller aldı. Pek çok konferans ve söyleşide konuşmacı olarak yer aldı.
+                Lisans eğitimini İ.T.Ü. Mimarlık Fakültesi, Mimarlık Bölümü’nde tamamladı. 
+                Mezuniyeti sonrasında, çeşitli mimarlık ofislerinde, kentsel tasarım, konut, ofis ve mağaza tasarımı gibi farklı fonksiyon ve ölçekler üzerine çalışma imkanı buldu, 
+                deneyim kazandı. Eş zamanlı olarak mobilya ve ürün tasarımına ağırlık verdi. Mimarlık, iç mimarlık ve ürün tasarımı çalışmalarını tek bir isim altında sürdürme isteğiyle, 
+                2014 yılında Cemal Çobanoğlu ile “ABRA Design Studio”yu kurdu. Kadir Has Üniversitesi, MEF Üniversitesi ve Beykent Üniversitesi’nde temel tasarım, mobilya tasarımı, 
+                mobilya tarihi ve ileri sunum teknikleri dersleri verdi. Tasarımları yurt içinde ve yurt dışında pek çok farklı sergi ve yayında yer aldı. Mobilya tasarımı yarışmalarında 
+                çeşitli ödüller aldı. Pek çok konferans ve söyleşide konuşmacı olarak yer aldı.
             </p>
                 <a href="" target="_blank" class="text-gray-400 hover:text-gray-500">
                 <span class="sr-only">LinkedIn</span>
@@ -125,20 +131,17 @@ export default function Home() {
             />
             <h3 className="text-xl font-semibold text-gray-800">Cemal Çobanoğlu</h3>
             <p className="mt-2 text-base leading-6 text-gray-600 p-12">
-                Lisans eğitimini İ.T.Ü. Mimarlık Fakültesi, İç Mimarlık Bölümü’nde, yüksek lisans eğitimini ise IMIAD’da tamamladı. IMIAD kapsamında İ.T.Ü ile birlikte, Finlandiya’da Lahti University of Applied Sciences’da, İskoçya’da Edinburgh College of Art’da eğitim aldı. Bu süreçte yüzyıl ortası mobilya tasarımı ve İskandinav tasarımı üzerine tezini yazdı. Katıldığı kentsel tasarım, iç mimarlık ve mobilya tasarımı yarışmalarında birçok farklı ödül aldı. Mezuniyeti sonrasında, çeşitli iç mimarlık ve tasarım ofislerinde deneyim kazandı. 2014 senesinde Başak Bakkaloğlu ile birlikte “ABRA Design Studio”yu kurdu. Tasarım yarışmalarında jüri başkanı ve jüri üyesi olarak bulundu, yurt içi ve yurt dışındaki tasarım haftalarında ve sergilerinde katılımcı ve küratör olarak yer aldı. Farklı mimarlık ve tasarım yayınlarında köşe yazarlığı yaptı, üniversitelerde mobilya tasarımı, mobilya tarihi ve temel tasarım dersleri verdi. Pek çok farklı konferans ve söyleşide konuşmacı ve moderatör olarak yer aldı.
+                Lisans eğitimini İ.T.Ü. Mimarlık Fakültesi, İç Mimarlık Bölümü’nde, yüksek lisans eğitimini ise IMIAD’da tamamladı. IMIAD kapsamında İ.T.Ü ile birlikte, 
+                Finlandiya’da Lahti University of Applied Sciences’da, İskoçya’da Edinburgh College of Art’da eğitim aldı. Bu süreçte yüzyıl ortası mobilya tasarımı ve İskandinav 
+                tasarımı üzerine tezini yazdı. Katıldığı kentsel tasarım, iç mimarlık ve mobilya tasarımı yarışmalarında birçok farklı ödül aldı. Mezuniyeti sonrasında, çeşitli iç mimarlık 
+                ve tasarım ofislerinde deneyim kazandı. 2014 senesinde Başak Bakkaloğlu ile birlikte “ABRA Design Studio”yu kurdu. Tasarım yarışmalarında jüri başkanı ve jüri üyesi 
+                olarak bulundu, yurt içi ve yurt dışındaki tasarım haftalarında ve sergilerinde katılımcı ve küratör olarak yer aldı. Farklı mimarlık ve tasarım yayınlarında köşe 
+                yazarlığı yaptı, üniversitelerde mobilya tasarımı, mobilya tarihi ve temel tasarım dersleri verdi. Pek çok farklı konferans ve söyleşide konuşmacı ve moderatör olarak yer aldı.
             </p>
                 <a href="" target="_blank" class="text-gray-400 hover:text-gray-500">
                 <span class="sr-only">LinkedIn</span>
-                <svg class="w-8 h-6 justify-self-center" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
-                  <g clip-path="url(#clip0_4062_37004)">
-                  <path fill-rule="evenodd" d="M20.7076 20.4751H17.163V14.9241C17.163 13.6004 17.1394 11.8964 15.3194 11.8964C13.4733 11.8964 13.1908 13.3386 13.1908 14.8278V20.4748H9.64618V9.05963H13.049V10.6196H13.0966C13.4372 10.0374 13.9293 9.55839 14.5205 9.23369C15.1118 8.90895 15.78 8.7507 16.4541 8.77569C20.0467 8.77569 20.7091 11.1388 20.7091 14.213L20.7076 20.4751ZM5.64668 7.49926C5.23986 7.49934 4.84212 7.37878 4.50383 7.15281C4.16551 6.92684 3.9018 6.60565 3.74606 6.22981C3.59029 5.85397 3.54948 5.44039 3.62878 5.04137C3.70807 4.64234 3.90391 4.27577 4.19154 3.98802C4.47917 3.70031 4.84563 3.50434 5.24466 3.4249C5.64365 3.34546 6.05726 3.38612 6.43314 3.54174C6.80905 3.69736 7.13036 3.96096 7.35644 4.29918C7.58252 4.63739 7.70322 5.03509 7.70329 5.44191C7.70333 5.71204 7.6502 5.97954 7.54685 6.22911C7.44354 6.47871 7.29204 6.70549 7.10108 6.89653C6.91011 7.08757 6.68337 7.23914 6.43384 7.34256C6.18428 7.44598 5.91681 7.49922 5.64668 7.49926ZM7.41899 20.4751H3.87068V9.05963H7.41899V20.4751ZM22.4747 0.0936092H2.08951C1.62683 0.0883878 1.18099 0.267072 0.849979 0.590397C0.518971 0.913721 0.329866 1.35524 0.324219 1.81792V22.2877C0.329672 22.7506 0.518661 23.1924 0.849658 23.5161C1.18066 23.8398 1.6266 24.0188 2.08951 24.0138H22.4747C22.9386 24.0197 23.3857 23.8412 23.718 23.5175C24.0504 23.1939 24.2406 22.7515 24.247 22.2877V1.81644C24.2404 1.35284 24.0501 0.910826 23.7178 0.587509C23.3854 0.264192 22.9383 0.086018 22.4747 0.0921323V0.0936092Z"/>
-                  </g>
-                  <defs>
-                  <clipPath id="clip0_4062_37004">
-                  <rect width="24.3692" height="24" fill="white" transform="translate(0 0.0921631)"/>
-                  </clipPath>
-                  </defs>
-                  </svg>
+                <FaLinkedin className="w-8 h-6 justify-self-center" />
+
             </a>
         </div>
     </div>
