1995fcda136c4c6f1b2ced94ed8dca83ef70773b
keremakagunduz <keremakagunduz@gmail.com>
2026-02-28 15:37:59 +0300

    layout

--- abraapp/src/components/Carousel.jsx
+++ abraapp/src/components/Carousel.jsx
@@ -39,29 +39,27 @@ const Carousel = ({ projects }) => {
 
   return (
     <>
-<article className="flex items-center">
-  <div className="relative h-screen flex justify-center overflow-hidden">
+        <article className="items-center">
+          <div className="relative h-screen flex justify-center text-[3rem]">
+      
+            <img
+              src={imageLoader(projects[currentIndex].cover_image, 1200)} // Fallback for older browsers
+              srcSet={`
+                ${imageLoader(projects[currentIndex].cover_image, 400)} 400w,
+                ${imageLoader(projects[currentIndex].cover_image, 800)} 800w,
+                ${imageLoader(projects[currentIndex].cover_image, 1200)} 1200w,
+                ${imageLoader(projects[currentIndex].cover_image, 1600)} 1600w
+              `}
+              sizes="(max-width: 400px) 400px,
+                    (max-width: 800px) 800px,
+                    (max-width: 1200px) 1200px,
+                    1600px" // Default for larger screens
+              alt={projects[currentIndex].title}
+              loading="lazy"
+            />
 
-    <img
-      src={imageLoader(projects[currentIndex].cover_image, 1200)} // Fallback for older browsers
-      srcSet={`
-        ${imageLoader(projects[currentIndex].cover_image, 400)} 400w,
-        ${imageLoader(projects[currentIndex].cover_image, 800)} 800w,
-        ${imageLoader(projects[currentIndex].cover_image, 1200)} 1200w,
-        ${imageLoader(projects[currentIndex].cover_image, 1600)} 1600w
-      `}
-      sizes="(max-width: 400px) 400px,
-             (max-width: 800px) 800px,
-             (max-width: 1200px) 1200px,
-             1600px" // Default for larger screens
-      alt={projects[currentIndex].title}
-      loading="lazy"
-      className="absolute inset-0 object-cover w-full h-full"
-    />
-
-  </div>
-</article>
-
+          </div>
+        </article>
 
       <div className="absolute inset-0 bg-white bg-opacity-50 flex w-full h-fit place-self-center pt-24 pb-24">
         <div className="flex absolute self-center justify-between text-4xl w-full h-full">
