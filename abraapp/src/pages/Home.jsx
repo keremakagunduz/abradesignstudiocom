4101d2b64de7c3575b4db1a3f43d7e517fdcde8a
keremakagunduz <keremakagunduz@gmail.com>
2026-02-28 21:54:02 +0300

    icon logo

--- abraapp/src/pages/Home.jsx
+++ abraapp/src/pages/Home.jsx
@@ -91,7 +91,7 @@ export default function Home() {
   <img
       src={imageLoader("https://d3hojcyp0aupte.cloudfront.net/asset/Abra_Icon_png.png", 400)}
       alt="Abra Design Studio"
-      className="max-w-10 mx-auto"
+      className="max-w-20 ml-0 mr-auto"
   />
 
 <div id="about-us" className="block px-6 py-12 lg:px-48">
