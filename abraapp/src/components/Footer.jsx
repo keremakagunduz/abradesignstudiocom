a8e8b6bf80d08da6e69e2a227077b0050f719cdf
keremakagunduz <keremakagunduz@gmail.com>
2026-03-13 23:21:59 +0300

    ios center

--- abraapp/src/components/Footer.jsx
+++ abraapp/src/components/Footer.jsx
@@ -13,26 +13,26 @@ const Footer = () => (
       lineHeight: "1.5",
     }}
   >
-  
-    <div class="max-w-screen-xl px-4 mx-auto overflow-hidden sm:px-6 lg:px-8 place-items-center">
+  <div className="max-w-screen-xl px-4 mx-auto overflow-hidden sm:px-6 lg:px-8 flex flex-col items-center justify-center text-center">
 
-        <div className="w-fit flex">
+    <div className="w-fit flex items-center">
         <BsEnvelope className="self-center" />
-        <p className="ml-2 text-base leading-6 text-center text-gray-400">info@abradesignstudio.com</p>
-        </div>
-
-        <div className="w-fit flex">
-        <FaPhoneAlt className="self-center"/>
-        <p className="ml-2 text-base leading-6 text-center text-gray-400">+90 505 251 8358</p>
-        </div>
-
-        <p className="pt-6 mt-1 text-base leading-6 text-center text-gray-400">
-          Copyrights of all pictures published on this website are reserved by
-          Abra Design Studio. No material published here can be copied, reproduced,
-          posted, used in any way without my written permission.
-        </p>
-
+        <p className="ml-2 text-base leading-6 text-gray-400">info@abradesignstudio.com</p>
     </div>
+
+    <div className="w-fit flex items-center">
+        <FaPhoneAlt className="self-center" />
+        <p className="ml-2 text-base leading-6 text-gray-400">+90 505 251 8358</p>
+    </div>
+
+    <p className="pt-6 mt-1 text-base leading-6 text-gray-400">
+        Copyrights of all pictures published on this website are reserved by
+        Abra Design Studio. No material published here can be copied, reproduced,
+        posted, used in any way without my written permission.
+    </p>
+
+</div>
+
 
   </footer>
 );
