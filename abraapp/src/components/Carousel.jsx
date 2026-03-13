74938c319c6859f78f070209cd30ec1bef478ccb
keremakagunduz <keremakagunduz@gmail.com>
2026-03-14 00:01:58 +0300

    carousel layout

--- abraapp/src/components/Carousel.jsx
+++ abraapp/src/components/Carousel.jsx
@@ -49,24 +49,25 @@ const Carousel = ({ projects }) => {
   return (
     <>
       <article className="items-center">
-        <div className="relative h-screen flex justify-center text-[3rem]">
-          <img
-            src={imageLoader(projects[currentIndex].cover_image, 1200)} // Fallback for older browsers
-            srcSet={`
-              ${imageLoader(projects[currentIndex].cover_image, 400)} 400w,
-              ${imageLoader(projects[currentIndex].cover_image, 800)} 800w,
-              ${imageLoader(projects[currentIndex].cover_image, 1200)} 1200w,
-              ${imageLoader(projects[currentIndex].cover_image, 1600)} 1600w
-            `}
-            sizes="(max-width: 400px) 400px,
-                  (max-width: 800px) 800px,
-                  (max-width: 1200px) 1200px,
-                  1600px" // Default for larger screens
-            alt={projects[currentIndex].title}
-            loading="lazy"
-            className="w-full min-h-screen object-cover"
-          />
-        </div>
+<div className="relative min-h-screen flex justify-center overflow-hidden">
+    <img
+        src={imageLoader(projects[currentIndex].cover_image, 1200)} 
+        srcSet={`
+            ${imageLoader(projects[currentIndex].cover_image, 400)} 400w,
+            ${imageLoader(projects[currentIndex].cover_image, 800)} 800w,
+            ${imageLoader(projects[currentIndex].cover_image, 1200)} 1200w,
+            ${imageLoader(projects[currentIndex].cover_image, 1600)} 1600w
+        `}
+        sizes="(max-width: 400px) 400px,
+               (max-width: 800px) 800px,
+               (max-width: 1200px) 1200px,
+               1600px"
+        alt={projects[currentIndex].title}
+        loading="lazy"
+        className="w-full min-h-screen object-cover"
+    />
+</div>
+
 
         <div className="absolute inset-0 self-end w-fit bg-white bg-opacity-50 p-8 text-4xl rounded">
           <button onClick={scrollToElement}>Biz Kimiz?</button>
