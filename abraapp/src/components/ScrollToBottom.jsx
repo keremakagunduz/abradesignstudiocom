18f2de27906d668eccd2e49e53ebaab1cc71dafd
keremakagunduz <keremakagunduz@gmail.com>
2026-05-02 22:32:39 +0300

    contact icons

--- abraapp/src/components/ScrollToBottom.jsx
+++ abraapp/src/components/ScrollToBottom.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { FaAngleUp,FaPhoneAlt,FaInstagram } from "react-icons/fa";
 import { BsEnvelope } from "react-icons/bs";
-import { PiEnvelopeThin } from "react-icons/pi";
-import { IoCallOutline } from "react-icons/io5";
 
 import "../index.css";
 
@@ -19,8 +17,8 @@ const ScrollToBottom = () => {
             <a href="https://www.instagram.com/abradesignstudio/" target="_blank" className="flex justify-center text-gray-400 hover:text-gray-500 mr-4">
                 <FaInstagram className="max-w-8 self-end cursor-pointer" />
             </a>
-            <PiEnvelopeThin onClick={goToBottom} className="max-w-8 self-end cursor-pointer mr-4 ml-4" />
-            <IoCallOutline onClick={goToBottom} className="max-w-8 self-end cursor-pointer ml-4" />
+            <BsEnvelope onClick={goToBottom} className="max-w-8 self-end cursor-pointer mr-4 ml-4" />
+            <FaPhoneAlt onClick={goToBottom} className="max-w-8 self-end cursor-pointer ml-4" />
         </div>
         </>
     );
