df68a07a975dc87b73395485e2e12adef971820f
keremakagunduz <keremakagunduz@gmail.com>
2026-05-03 10:59:00 +0300

    contact icons

--- abraapp/src/components/ScrollToBottom.jsx
+++ abraapp/src/components/ScrollToBottom.jsx
@@ -17,8 +17,8 @@ const ScrollToBottom = () => {
             <a href="https://www.instagram.com/abradesignstudio/" target="_blank" className="flex justify-center text-gray-400 hover:text-gray-500 mr-4">
                 <FaInstagram className="max-w-8 self-end cursor-pointer" />
             </a>
-            <BsEnvelope onClick={goToBottom} className="max-w-8 self-end cursor-pointer mr-4 ml-4" />
-            <FaPhoneAlt onClick={goToBottom} className="max-w-8 self-end cursor-pointer ml-4" />
+            <BsEnvelope onClick={goToBottom} className="max-w-8 self-end cursor-pointer mr-4 ml-4 fill-transparent" />
+            <FaPhoneAlt onClick={goToBottom} className="max-w-8 self-end cursor-pointer ml-4 [stroke:#6b7280] [stroke-width:25] fill-transparent" />
         </div>
         </>
     );
