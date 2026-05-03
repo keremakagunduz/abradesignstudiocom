0b31664e08ecb44513df499316ed7840297e2caa
keremakagunduz <keremakagunduz@gmail.com>
2026-05-03 14:09:34 +0300

    studio - team

--- abraapp/src/pages/Home.jsx
+++ abraapp/src/pages/Home.jsx
@@ -169,11 +169,11 @@ const cemal_cv = userLocale.startsWith('tr') ? (
   </button>
   <div className="block px-6 py-12 pt-24 sm:pt-12 lg:px-48">
 
-    <h2 className="text-3xl font-bold underline underline-offset-4 text-gray-700 mb-6">Studio<span aria-hidden="true">&nbsp;&nbsp;&nbsp;</span></h2>
+    <h2 className="text-3xl font-bold underline underline-offset-4 text-gray-700 mb-6">Studio<span aria-hidden="true">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></h2>
     {about_text}
 
 <div className="flex flex-col xl:flex-row justify-center mt-12 space-y-12 xl:space-y-0 xl:space-x-12">
-    <h2 className="text-3xl font-bold underline underline-offset-4 text-gray-700 mb-6">Team<span aria-hidden="true">&nbsp;&nbsp;&nbsp;</span></h2>
+    <h2 className="text-3xl font-bold underline underline-offset-4 text-gray-700 mb-6">Team<span aria-hidden="true">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></h2>
 
     <div className="block text-center bg-white shadow-lg rounded-lg p-6 border border-gray-200">
         <img 
