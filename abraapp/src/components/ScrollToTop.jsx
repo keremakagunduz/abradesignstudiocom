63cd49a53bbb9988ade8a8af44747a3a1dc24d5e
keremakagunduz <keremakagunduz@gmail.com>
2026-07-05 13:30:32 +0300

    scrolltotop

--- abraapp/src/components/ScrollToTop.jsx
+++ abraapp/src/components/ScrollToTop.jsx
@@ -9,16 +9,6 @@ const imageLoader = (src, width, quality) => {
 
 
 const ScrollToTop = () => {
-    const [showTopBtn, setShowTopBtn] = useState(false);
-    useEffect(() => {
-        window.addEventListener("scroll", () => {
-            if (window.scrollY > 400) {
-                setShowTopBtn(true);
-            } else {
-                setShowTopBtn(false);
-            }
-        });
-    }, []);
     const goToTop = () => {
         window.scrollTo({
             top: 0,
@@ -28,14 +18,14 @@ const ScrollToTop = () => {
     return (
         <div className="btm-to-top">
             {" "}
-            {showTopBtn && (
+            {
                 <img
             src={imageLoader("https://d3hojcyp0aupte.cloudfront.net/asset/Abra_Icon_png.png", 400)}
             alt="Abra Design Studio"
             className="max-w-40 ml-0 mr-auto absolute pt-0 sm:pt-6"
             onClick={goToTop}
             />
-            )}{" "}
+            }{" "}
         </div>
     );
 };
